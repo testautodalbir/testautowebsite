bc9c1e7d6851f9ae945f8246e79d24a90b217378
Dalbir <58086523+dasing21@users.noreply.github.com>
2024-06-28 17:30:21 +0200

    .env port and server.js update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
