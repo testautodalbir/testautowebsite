1e3c91c2a3a0d510d3e66e170f7dc55281ec5bbb
Dalbir <58086523+dasing21@users.noreply.github.com>
2024-06-29 18:07:15 +0200

    cors config and form update

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
